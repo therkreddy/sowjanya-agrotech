3e6cf13939bf352fd30b1928e586b115989c6132
rkbairi <rkbairi@mears.com>
2017-07-06 10:56:32 -0400

    initial

---
+++ src/main/java/com/agrotech/AppController.java
@@ -0,0 +1,15 @@
+package com.agrotech;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Controller
+@EnableAutoConfiguration
+@ComponentScan
+public class AppController {
+
+}
